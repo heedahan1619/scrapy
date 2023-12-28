5ef9bb34d8b71eaa51726dc67c60ac73dfd09bfc
hee <heedahan1619@gmail.com>
2023-12-28 20:03:15 +0900

    새로운 사이트 구조 파악

--- self_study_scrapy/self_study_scrapy/spiders/news_spider.py
+++ self_study_scrapy/self_study_scrapy/spiders/news_spider.py
@@ -348,6 +348,337 @@ class Stella1Spider(scrapy.Spider):
         yield item
 
 
+
+
+
+
+
+
+
+from datetime import datetime, timedelta
+
+import re
+import os
+import sys
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from items import CrawlerNewsItem
+
+class SmedailySpider(scrapy.Spider):
+    
+    """스파이더 설정"""
+    # 스파이더 이름
+    name = "smedaily"    
+    
+    """언론사"""
+    # 언론사
+    origin_media = "중소기업신문(Smedaily)"
+    
+    # 언론사명
+    media = str(origin_media.split("(")[0])
+    
+    language = "ko" 
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+    
+    """url"""
+    # 메인 홈페이지 url
+    main_url = "https://www.smedaily.co.kr"
+    
+    # 카테고리 뉴스 목록 url
+    category_news_list_url = "https://www.smedaily.co.kr{}&page={}"
+    
+    # 페이지 요청 범위
+    page_request_range = 10
+    
+    #  언론사 카테고리 dict
+    media_category_dict = {} # 대분류 카테고리 dict
+
+
+    def __init__(
+            self, 
+            
+            # 날짜 입력
+            start_date_input = None,
+            end_date_input = None,
+   
+            save_method="json"
+        ):
+
+        # 날짜 입력
+        if start_date_input is None:
+            start_date_input = input("시작 날짜 입력(YYYYMMDD): ")
+        if end_date_input is None:
+            end_date_input = input("종료 날짜 입력(YYYYMMDD): ")
+            
+        # 날짜 변환 및 유효성 확인
+        def transform_date(date_input):
+            try:
+                return datetime.strptime(date_input, "%Y%m%d")
+            except ValueError:
+                print("올바른 날짜 형식이 아닙니다.")
+                return None
+
+        # 날짜 datetime 형태 변환
+        def transform_date_output(date):
+            return date.strftime("%Y-%m-%d")
+        
+        self.start_date_input = start_date_input
+        self.start_date_input = end_date_input
+        self.start_date = transform_date(start_date_input)
+        self.end_date = transform_date(end_date_input) + timedelta(days=1) - timedelta(seconds=1)
+
+        # start_date와 end_date의 유효성을 확인
+        assert self.start_date is not None and self.end_date is not None, "유효하지 않은 날짜 형식이 입력되었습니다."
+        assert self.start_date.timestamp() <= self.end_date.timestamp(), "end_date가 start_date보다 앞에 있습니다."
+
+        self.save_method = save_method
+        assert self.save_method in ("json"), "선택할 수 있는 save_method가 아닙니다."
+
+
+    def start_requests(self):
+        """크롤러가 시작하면서 실행하는 메소드"""
+        
+        print("\n")  
+        print(f"언론사: {self.media}")
+        print("\n")  
+        
+        return [
+            scrapy.Request(
+                
+                # 메인 홈페이지로 이동
+                url=self.main_url,
+                
+                headers=self.headers,
+                
+                # 언론사 카테고리 추출 함수로 연결
+                callback=self.parse_media_category
+            )
+        ]
+        
+
+    def parse_media_category(self, response):   
+        """언론사 카테고리 추출 함수"""   
+
+        input_media_category = ""
+
+        """카테고리 대분류"""
+        for category_info in response.xpath("//ul[@id='user-menu']/li/a"):
+            # print(f"{self.media} 대분류 카테고리 정보: {category_info}")
+            
+            # 대분류 카테고리 코드
+            media_category_code = category_info.xpath("@href").get()
+            # print(f"{self.media} 대분류 카테고리 코드: {media_category_code}")  
+            
+            # 대분류 카테고리 이름
+            media_category = category_info.xpath("text()").get()
+            input_media_category += (media_category + ", ")
+            # print(f"{self.media} 대분류 카테고리: {media_category}")
+            # print("--------------------------------")
+        
+            """for문 밖 출력 확인"""   
+            # 대분류 카테고리 dict
+            self.media_category_dict[media_category] = media_category_code
+        print(f"전체 카테고리 dict: {self.media_dict}")
+        print(f"{self.media} 대분류 카테고리 dict: {self.media_category_dict}")
+        
+    
+        """카테고리 입력"""
+        # 카테고리 입력 시 카테고리 정보 제공을 위한 문자열
+        input_media_category = input_media_category[:-2]     
+        # print(f"선택할 카테고리 문자열: {input_media_category}")
+        
+        # 카테고리 선택
+        select_media_category = input(f"\n[{input_media_category}] 중에서 카테고리를 선택하세요: \t").split(", ")
+        print(f"선택한 카테고리 전체:  {select_media_category}")
+        
+        # 선택한 카테고리가 언론사 카테고리에 존재하는지 확인
+        selected_media_category_list = []
+        
+        for selected_media_category in select_media_category:
+            print(f"선택한 카테고리:  {selected_media_category}")
+
+        """스크래피 요청"""    
+        # 카테고리 추출
+        for media_category_code, media_category in self.media_category_dict.items():
+            # print(f"{self.media} 대분류 카테고리 코드: {media_category_code}")
+            # print(f"{self.media} 대분류 카테고리: {media_category}")
+            # print("--------------------------------")
+            
+            # 페이지 지정
+            for page_num in range(1, self.page_request_range+1):
+                # print(f"페이지 번호: {page_num}")
+                
+                # print(self.category_news_list_url.format(media_category_code, page_num))
+                        
+                # 스크래피 요청
+                yield scrapy.Request(
+                    
+                    # 카테고리 뉴스 목록 url
+                    url = self.category_news_list_url.format(media_category_code, page_num),
+                    
+                    headers=self.headers,
+                    
+                    # 카테고리별 뉴스 목록 페이지 확인 함수로 연결
+                    callback=self.parse_category_news_list,
+                    
+                    meta={
+                        "media_category_code": media_category_code,
+                        "media_category": media_category,
+                        "media_category_sub": media_category
+                    }
+                )
+                   
+    
+    def parse_category_news_list(self, response):
+        """카테고리별 뉴스 목록 확인 함수"""
+        
+        # print(f"{self.media} 카테고리 뉴스 목록 url: {response.url}")
+        
+        media_category_code = response.meta["media_category_code"]
+        media_category = response.meta["media_category"]
+        media_category_sub = response.meta["media_category_sub"]
+        
+        # print(f"\n{self.media} 카테고리 대분류: {media_category}")
+        # print(f"{self.media} 카테고리 소분류: {media_category_sub}")
+        # print(f"{self.media} 카테고리 대분류 코드: {media_category_code}")
+        
+        
+        # 페이지 처리
+        if response.url == self.main_url + media_category_code:
+            page_num = 1
+        else:
+            page_num = (response.url).split('=')[-1]
+        # print(f"페이지 번호: {page_num}")
+        # print(f"\카테고리 뉴스 목록 url: {response.url}")
+        # print("--------------------------------")
+                                                          
+        """카테고리별 뉴스 목록 확인"""
+        is_next_news_list = False
+        
+     
+        # 카테고리별 뉴스 목록 확인
+        for news_list in response.xpath("//section[@id='section-list']//div[@class='view-cont']"):
+            # print(f"\n뉴스 목록 정보: {news_list}")
+        
+            # 날짜 추출
+            date = news_list.xpath("span[@class='byline']/em[3]/text()").get()
+            # print(f"{self.media} 카테고리별 뉴스 목록 날짜: {date}")
+            
+            date = datetime.strptime(date, "%Y.%m.%d %H:%M")
+            # print(f"{self.media} 카테고리별 뉴스 목록 datetime 날짜: {date}")
+        
+            # 뉴스 기사 url
+            news_post_url = self.main_url + news_list.xpath("h4[@class='titles']/a/@href").get()
+            # print(f"{self.media} 뉴스 기사 url: {news_post_url}")
+            
+            # 입력한 날짜의 기간 범위 안
+            if self.start_date <= date <= self.end_date:
+                
+                is_next_news_list = True
+                
+                yield scrapy.Request(
+                    
+                    # 뉴스 기사 url
+                    url=news_post_url,
+                    
+                    headers=self.headers,
+                    
+                    # 뉴스 기사 항목 추출 함수로 연결
+                    callback=self.parse_category_news_post,
+                    
+                    meta={
+                        "date": date,
+                        "media_category_code": media_category_code,
+                        "media_category": media_category,
+                        "media_category_sub": media_category_sub
+                    }
+                )
+                
+            # 입력한 날짜의 기간 범위 밖
+            elif self.end_date < date:
+                is_next_news_list = True
+                
+            # 카테고리별 뉴스 목록의 다음 페이지 확인
+            if is_next_news_list == True:
+                next_page_num = int(page_num)+self.page_request_range
+                # print(f"다음 페이지 번호: {next_page_num}")
+                
+                yield scrapy.Request(
+                    
+                    # 카테고리 뉴스 목록 url
+                    url=self.category_news_list_url.format(media_category_code, str(next_page_num)),
+                    
+                    headers=self.headers,
+                    
+                    # 카테고리별 뉴스 목록 페이지 확인 함수로 연결
+                    callback=self.parse_category_news_list,
+                    
+                    meta={
+                            "media_code": media_category_code,
+                            "media_category": media_category,
+                            "media_category_sub":media_category_sub
+                    } 
+                )   
+            
+
+    def parse_category_news_post(self, response):
+        """뉴스 기사 항목 추출 함수"""
+        
+        print(f"\n 뉴스 기사 url: {response.url}")
+        
+        item = CrawlerNewsItem()
+    
+        # 언론사 카테고리 대분류
+        item["media_category"] = response.meta["media_category"]
+        print(f" 기사 카테고리 대분류: {item["media_category"]}")
+        
+        # 언론사 카테고리 소분류
+        item["media_category_sub"] = response.meta["media_category_sub"]
+        print(f" 기사 카테고리 소분류: {item["media_category_sub"]}")
+        
+        # 기사 날짜
+        item["inp_date"] = response.meta["date"]
+        print(f" 기사 날짜: {item["inp_date"]}")
+        
+        # 기사 제목
+        item["title"] = response.xpath("//header[@class='article-view-header']/h3[@class='heading']/text()").get().strip()
+        print(f" 기사 제목: {item["title"]}")
+        
+        # 기사 내용
+        item["content"] = " ".join(response.xpath("//div[@class='article-body']/article/p/text()").getall()).replace("\n", "").strip()    
+        print(f" 기사 내용: {item["content"]}")
+
+        
+        # 핀인사이트 카테고리 대분류
+        item["category"] = ""
+        # 핀인사이트 카테고리 소분류
+        item["category_sub"] = ""
+        
+        item["save_method"] = self.save_method
+        item["origin_nm"] = self.origin_media
+        item["origin_url"] = response.url
+        item["language"] = self.language
+
+        print("--------------------------------")   
+        yield item
+
+
+# #  vscode의 파이썬 Extension으로 디버깅을 하기 위한 세팅입니다.
+# if __name__ == "__main__":
+#     settings = get_project_settings()
+#     settings["LOG_LEVEL"] = "DEBUG"
+#     settings["ITEM_PIPELINES"] = {
+#         "crawler_news.pipelines.CrawlerNewsPipeline":300
+#     }
+#     process = CrawlerProcess(settings=settings)
+#     process.crawl(SmedailySpider)
+#     process.start()
+
+
 #  vscode의 파이썬 Extension으로 디버깅을 하기 위한 세팅입니다.
 if __name__ == "__main__":
     settings = get_project_settings()
